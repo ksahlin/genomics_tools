9e205ea5c3198aef16498a2698d5021c089bd262
ksahlin <krsahlin@gmail.com>
2014-09-26 13:43:42 +0100

    added main simulation script

---
+++ scripts/main_simulate.py
@@ -0,0 +1,69 @@
+import os
+
+import argparse
+from genomics_tools.file_formats import bam
+from genomics_tools.simulate import genome,contigs,reads
+from mapping import align
+
+def simulate_instance(args):
+    print 'here'
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+    genome_path = os.path.join(args.output_path, 'genome.fa')
+    contig_path = os.path.join(args.output_path, 'ctgs.fa')
+    read1_path = os.path.join(args.output_path, 'reads1.fa')
+    read2_path = os.path.join(args.output_path, 'reads2.fa')
+    bam_path = os.path.join(args.output_path, 'mapped')
+
+    #genome
+    g = genome.Genome([0.25]*4,args.genomelen,'genome1')
+    g.genome()
+    print >> open(genome_path,'w'), g.genome_fasta_format()
+    #contigs
+    ctgs = open(contig_path,'w')
+    for i,x in enumerate(range(0,args.genomelen,(args.contiglen + args.gaplen))):
+        ctgs.write('>ctg{0}\n{1}\n'.format(i,g.sequence[x:x+args.contiglen]))
+
+    #reads
+    lib = reads.DNAseq(args.read_length ,args.coverage, args.mean,args.sd)
+    lib.simulate_pe_reads(g)
+    reads1 = open(read1_path,'w')
+    reads2 = open(read2_path,'w')
+    i=0
+    for read in lib.fasta_format():
+        if i%2==0:
+            reads1.write(read)
+        else:
+            reads2.write(read)
+        i+=1
+
+    #mapping
+    align.map_paired_reads(read1_path, read2_path, contig_path, bam_path, args)
+
+def main(args):
+	simulate_instance(args)
+
+if __name__ == '__main__':
+    ##
+    # Take care of input
+    parser = argparse.ArgumentParser(description="Simulate paired end reads with a given mean and standard deviation on mean insert size. A given coverage should also be provided.")
+    parser.add_argument('genomelen', type=int, help='Length of genome. ')
+    parser.add_argument('contiglen', type=int, help='Length of contigs. ')
+    parser.add_argument('gaplen', type=int, help='Length of gap. ')
+
+    parser.add_argument('mean', type=int, help='Mean insert. ')
+    parser.add_argument('sd', type=int, help='Stddev insert.')
+    parser.add_argument('coverage', type=int, help='Coverage. ')
+    parser.add_argument('read_length', type=int, help='Length of read fragments within a pair. ')
+    parser.add_argument('output_path', type=str, help='path to folder output. ')
+    parser.add_argument( '-sort', dest='sort', action='store_true', default=False, help='Coordinate sort the reads in the bam file' )
+    parser.add_argument( '-sam', dest='sam', action='store_true', default=False, help='Output a samfile (default is bam)' )
+
+    #parser.add_argument('coverage', type=int, help='Coverage for read library. ')
+    #parser.add_argument('outpath', type=str, help='Path to output location. ')
+    #parser.add_argument('experiments', type=int, help='Number of experiment for each line in sim_in.txt file to run. ')
+    #parser.add_argument('genomelen', type=int, help='Length of the reference sequence. ')
+    #parser.add_argument('c_len', type=int, help='Contig length. ')
+
+    args = parser.parse_args()
+    main(args)
